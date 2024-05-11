c61ea42e788592f9a1160480232d3e1bab481f1c
Michael McGreal <mcgrealife@gmail.com>
2024-05-11 14:15:46 -0400

    cleanup

--- background.js
+++ background.js
@@ -1,19 +1,16 @@
-// Access chrome APIs here
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((message) => {
   if (message.action === "captureScreenshot") {
-    // Execute content script in the active tab to capture the screenshot
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTab = tabs[0]
       chrome.tabs.captureVisibleTab(activeTab.windowId, { format: "png" }, (dataUrl) => {
-        // Use the chrome.downloads API to download the screenshot
         chrome.downloads.download({
           url: dataUrl,
           filename: `${message.word}-${Date.now().toString()}.png`,
-          saveAs: false  // Change to true if you want to prompt the user to choose the download location
-        }, (downloadId) => {
-          console.log('Download started with ID:', downloadId)
+          saveAs: false
         })
       })
+
     })
+
   }
 })
--- content.js
+++ content.js
@@ -1,35 +1,28 @@
-// Function to log a message when the text content includes "The word was"
-const logMessageWhenWordFound = function (mutationsList, observer) {
+const notifyBackendWhenTheWordWas = function (mutationsList, observer) {
   for (const mutation of mutationsList) {
     if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-      // Check each added node for text content including "The word was"
       mutation.addedNodes.forEach(node => {
-        // Check if the node is a <span> element and contains the text "The word was"
-        // if (node.tagName === 'SPAN' && node.textContent.includes("The word was")) {
         if (node.textContent.includes("The word was")) {
-          const word = node.textContent.split("The word was ")[1]
-          console.log('the word was', word, '.', 'notifying chrome.runtime.sendMessage')
-          // Send message to background script to capture screenshot
-          chrome.runtime.sendMessage({ action: "captureScreenshot", word })
-          console.log('message sent to background.js.')
+          chrome.runtime.sendMessage(
+            {
+              action: "captureScreenshot",
+              word: node.textContent.split("The word was ")[1]
+            }
+          )
+          console.log('message sent to background.js')
         }
       })
     }
   }
 }
 
-// Select the parent element to observe
-const parentElement = document.querySelector("#game-chat > div > div.chat-content")
 
-// Create a MutationObserver instance
-const observer = new MutationObserver(logMessageWhenWordFound)
-
-// Start observing the parent element for changes in its child elements
-observer.observe(parentElement, { childList: true })
+const observer = new MutationObserver(notifyBackendWhenTheWordWas)
 
 
-// Disconnect the observer when the tab is closing
-window.onbeforeunload = function () {
-  observer.disconnect()
-  console.log('Observer disconnected. Tab is closing.')
-}
+observer.observe(
+  document.querySelector("#game-chat > div > div.chat-content"),
+  { childList: true }
+)
+
+window.onbeforeunload = () => observer.disconnect() 
