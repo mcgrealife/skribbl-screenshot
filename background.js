447a4b82b54c58962254523af1317b5dcf3d885e
Michael McGreal <mcgrealife@gmail.com>
2024-05-11 12:01:44 -0400

    4

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
-// Listen for messages from content script
+// Access chrome APIs here
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "captureScreenshot") {
     // Execute content script in the active tab to capture the screenshot
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTab = tabs[0]
+      console.log('chrome scripting', chrome.scripting)
       chrome.scripting.executeScript({
         target: { tabId: activeTab.id },
         function: () => {
--- content.js
+++ content.js
@@ -3,7 +3,8 @@ const observer = new MutationObserver((mutationsList, observer) => {
   for (const mutation of mutationsList) {
     if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
       for (const node of mutation.addedNodes) {
-        if (node.nodeType === Node.TEXT_NODE && node.textContent.includes("The word was")) {
+        // if (node.nodeType === Node.TEXT_NODE && node.textContent.includes("The word was")) {
+        if (node.nodeType === Node.TEXT_NODE) {
           // Send message to background script to capture screenshot
           chrome.runtime.sendMessage({ action: "captureScreenshot" })
           observer.disconnect()
